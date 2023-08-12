9e4ead680ea7d16467d150b541521b7362e7bc31
Levlav <levinlavatsa.com>
2023-08-12 09:50:24 -0700

    /AirBnB_clone.3.1

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-""" call to filestorage class """
+"""
+initialize the models package
+"""
+
+from os import getenv
 
 
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
